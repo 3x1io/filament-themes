e79c0d866a9bdb480fdd78421da46c7090b29dd7
Luis Escobar Bravo <luisescobarbravo@gmail.com>
2022-06-07 10:24:58 -0400

    Added migrations publish option with timestamp to prevent errors with spatie-laravel-settings

--- src/FilamentThemesProvider.php
+++ src/FilamentThemesProvider.php
@@ -23,13 +23,18 @@ class FilamentThemesProvider extends PluginServiceProvider
 
         $this->publishes([
             __DIR__ . '/../resources/views' => resource_path('views/'),
-            __DIR__ . '/../database/migrations' => base_path('database/settings'),
             __DIR__ . '/../publish' => public_path(),
         ], 'filament-themes');
 
         $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'filament-themes');
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         $this->loadViewsFrom(__DIR__ . '/../templates', 'filament-themes-templates');
+
+        if (! class_exists('ThemesSettings')) {
+            $this->publishes([
+                __DIR__ . '/../database/migrations/themes_settings.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_themes_settings.php'),
+            ], 'migrations');
+        }
     }
 
     protected function getPages(): array
